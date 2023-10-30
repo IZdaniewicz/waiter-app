2a61f9b76b3d3e62024d39f51ce2dd678e0a38ad
igor <igor.zdaniewicz@o2.pl>
2023-10-30 18:08:34 +0100

    Flyway database migration system

---
+++ src/main/resources/db/migration/V20231030173342__V1__init_migration.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS users (
+                       id SERIAL PRIMARY KEY,
+                       username VARCHAR(255) NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       role VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users_basic_info (
+                                  id SERIAL PRIMARY KEY,
+                                  name VARCHAR(255),
+                                  surname VARCHAR(255),
+                                  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS token (
+                                     id SERIAL PRIMARY KEY,
+                                     token VARCHAR(255) UNIQUE NOT NULL,
+                                     tokenType VARCHAR(20) DEFAULT 'BEARER',
+                                     revoked BOOLEAN NOT NULL,
+                                     expired BOOLEAN NOT NULL,
+                                     user_id INTEGER,
+                                     FOREIGN KEY (user_id) REFERENCES users(id)
+);
